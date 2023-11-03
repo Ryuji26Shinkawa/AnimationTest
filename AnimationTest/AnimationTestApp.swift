2460c4db7b477f9681ff5d7564c0445e4569becf
Ryuji26Shinkawa <manekinke03.playgaming@gmail.com>
2023-11-03 22:13:21 +0900

    初回Push

---
+++ AnimationTest/AnimationTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationTestApp.swift
+//  AnimationTest
+//
+//  Created by 新川竜司 on 2023/11/03.
+//
+
+import SwiftUI
+
+@main
+struct AnimationTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
